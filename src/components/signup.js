a71c448a280fb0845e7fdd40e4bebcf7b42ad749
abbas <abbasaffandi2002@gmail.com>
2023-12-05 09:38:21 -0800

    steps complete

--- src/components/signup.js
+++ src/components/signup.js
@@ -41,7 +41,7 @@ function signup() {
       <Navbar />
       <NavbarResponsive />
       <div className="py-10 max-md:px-2 md:px-24 flex justify-between">
-        <div class="w-[546.98px] text-white text-[45px] font-bold font-Poppins leading-[63px]">
+        <div class="w-[100%] text-white text-[45px] font-bold font-Poppins leading-[63px]">
           Sign Up
         </div>
         <img
