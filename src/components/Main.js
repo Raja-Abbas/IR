87962d1b3a2f561f30e39ab644467c9194088d32
abbas <abbasaffandi2002@gmail.com>
2023-12-08 12:51:13 -0800

    add

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './navbar';
-import Herosection from './herosection.js';
+import Herosection from './herosection';
 import CenterText from './centerText';
 import Slider from './slider';
 import { Deploy } from './deploy';
