0a11a4eca4dc86d3c82b18c865e7596150496a66
abbas <abbasaffandi2002@gmail.com>
2023-12-09 06:16:52 -0800

    first

--- src/components/footer/footerlinks.js
+++ src/components/footer/footerlinks.js
@@ -1,14 +1,31 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from "react";
 import RLogo from "../../images/r small footer.svg"
+import { useNavigate } from 'react-router-dom';
 
 
 function footerlinks() {
+  const navigate = useNavigate();
+  const handleLoginClick = () => {
+    navigate('/');
+  };
+
+  const navigateAbout = useNavigate();
+  const handleAboutClick = () => {
+    navigateAbout('/About');
+  };
+
+  const navigateBlog = useNavigate();
+  const handleBlogClick = () => {
+    navigateBlog('/Blog');
+  };
+
   return (
     <div className="min-[1750px]:me-[300px]">
     <div className="mt-4 mb-20 xl:mx-60 lg:mx-20 flex justify-between max-lg:flex-col lg:me-32">
       <div className="w-[185.18px] h-[76.63px] relative max-lg:mx-auto">
         <div className="w-[79.93px] h-[76.63px] left-0 top-0 absolute">
-          <div className="w-[68.06px] h-[68.06px] left-0 top-0 absolute">
+          <div className="w-[68.06px] h-[68.06px] left-0 top-0 absolute cursor-pointer" onClick={handleLoginClick}>
             <img src={RLogo} alt=""/>
           </div>
         </div>
@@ -19,7 +36,7 @@ function footerlinks() {
         </div>
       </div>
       <div className="mt-20 max-lg:text-center">
-      <div className="text-white text-sm font-normal font-Poppins leading-[35px]"><span className="hover:underline cursor-pointer">About Us</span><br/><span className="hover:underline cursor-pointer">How We Work</span><br/><span className="hover:underline cursor-pointer">Blog</span></div>
+      <div className="text-white text-sm font-normal font-Poppins leading-[35px]"><span className="hover:underline cursor-pointer" onClick={handleAboutClick}>About Us</span><br/><span className="hover:underline cursor-pointer">How We Work</span><br/><span className="hover:underline cursor-pointer" onClick={handleBlogClick}>Blog</span></div>
       </div>
        <div className="lg:mt-20 max-lg:text-center">
       <div class="text-white text-sm font-normal font-Poppins leading-[35px]"><span className="hover:underline cursor-pointer">Pricing</span><br/><span className="hover:underline cursor-pointer">Contact</span></div>
