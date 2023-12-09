5e6bb5ff6fa6b6cda283892fe546c17c8177504b
abbas <abbasaffandi2002@gmail.com>
2023-12-09 05:17:49 -0800

    first

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,53 +1,87 @@
-import React from 'react'
-import Image from "../images/R logo.svg"
-import Getstarted from '../common/Getstarted'
-import Login from '../common/Login'
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import Image from '../images/R logo.svg';
+import Getstarted from '../common/Getstarted';
+import Login from '../common/Login';
+import { useNavigate } from 'react-router-dom';
 
+function Navbar() {
+  const navigate = useNavigate();
+  const [isScrolled, setIsScrolled] = useState(false);
 
-function navbar() {
-   // eslint-disable-next-line react-hooks/rules-of-hooks
-   const navigate = useNavigate();
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const shouldSetScrolled = scrollPosition > 0;
+
+      setIsScrolled(shouldSetScrolled);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   const handleLoginClick = () => {
-    // Navigate to the Signup page
     navigate('/');
   };
 
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-   const navigateAbout = useNavigate();
-
+  const navigateAbout = useNavigate();
   const handleAboutClick = () => {
-    // Navigate to the Signup page
     navigateAbout('/About');
   };
-  
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const navigateBlog = useNavigate();
 
+  const navigateBlog = useNavigate();
   const handleBlogClick = () => {
-    // Navigate to the Signup page
     navigateBlog('/Blog');
   };
+
   return (
-    <div className='flex justify-between mx-4 me-6 my-4 items-center max-lg:hidden zindex'>
-    <div className='flex gap-2 items-center cursor-pointer'
-    onClick={handleLoginClick}>
-        <img src={Image} alt=''/>
-        <p className='text-white font-bold'>Intelligent<br/> Relations</p>
+    <div
+      className={`flex justify-between mx-4 me-6 my-4 items-center max-lg:hidden zindex ${
+        isScrolled ? 'fixed w-full my-0 mx-0 py-3 px-6 top-0 bg-white shadow-xl' : ''
+      }`}
+    >
+      <div
+        className='flex gap-2 items-center cursor-pointer'
+        onClick={handleLoginClick}
+      >
+        <img src={Image} alt='' />
+        <p className='text-white font-bold'>
+          Intelligent
+          <br /> Relations
+        </p>
+      </div>
+      <div className={`flex gap-6 items-center ${
+  isScrolled ? 'text-black' : 'text-white'
+}`}>
+        <p
+          className='text-[16px] font-[600] font-poppins cursor-pointer hover:underline'
+          onClick={handleAboutClick}
+        >
+          About
+        </p>
+        <p className='text-[16px] font-[600] font-poppins cursor-pointer hover:underline'>
+          How We Work
+        </p>
+        <p className='text-[16px] font-[600] font-poppins cursor-pointer hover:underline'>
+          Pricing
+        </p>
+        <p
+          className='text-[16px] font-[600] font-poppins cursor-pointer hover:underline'
+          onClick={handleBlogClick}
+        >
+          Blog
+        </p>
+        <p className='text-[16px] font-[600] font-poppins cursor-pointer hover:underline'>
+          Contact
+        </p>
+        <Login />
+        <Getstarted />
+      </div>
     </div>
-    <div className='flex gap-6 text-white items-center'>
-        <p className='text-[16px] font-[600] font-poppins cursor-pointer hover:underline'
-        onClick={handleAboutClick}>About</p>
-        <p className='text-[16px] font-[600] font-poppins cursor-pointer hover:underline'>How We Work</p>
-        <p className='text-[16px] font-[600] font-poppins cursor-pointer hover:underline'>Pricing</p>
-        <p className='text-[16px] font-[600] font-poppins cursor-pointer hover:underline' onClick={handleBlogClick}>Blog</p>
-        <p className='text-[16px] font-[600] font-poppins cursor-pointer hover:underline'>Contact</p>
-        <Login/>
-       <Getstarted/>
-    </div>
-    </div>
-  )
+  );
 }
 
-export default navbar
+export default Navbar;
